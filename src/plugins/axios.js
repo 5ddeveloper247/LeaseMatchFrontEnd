6c7f7919eed05db0791b22fcff39cd32c45fc705
5ddeveloper247 <127947055+5ddeveloper247@users.noreply.github.com>
2024-06-03 11:43:31 +0500

    commit

---
+++ src/plugins/axios.js
@@ -0,0 +1,12 @@
+import axios from 'axios';
+
+const axiosInstance = axios.create({
+  baseURL: 'http://127.0.0.1:8000/api/5d/v1', // Change this to your Laravel API URL
+  headers: {
+    'Content-Type': 'application/json/multipart/form-data',
+    'Accept': 'application/json',
+    'Content-Type': ''
+  }
+});
+
+export default axiosInstance;
