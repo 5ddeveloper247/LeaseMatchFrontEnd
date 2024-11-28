738affb872afadfbb558f0ac9700ed58568207c5
5ddeveloper247 <127947055+5ddeveloper247@users.noreply.github.com>
2024-11-28 13:51:22 +0500

    validation

--- src/plugins/axios.js
+++ src/plugins/axios.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  //  baseURL: 'http://127.0.0.1:8000/api/5d/v1', // Change this to your Laravel API URL
-  baseURL: 'http://lmb.gregorygadson.io/api/5d/v1', // Change this to your Laravel API URL
+  baseURL: 'http://127.0.0.1:8000/api/5d/v1', // Change this to your Laravel API URL
+  //baseURL: 'http://lmb.gregorygadson.io/api/5d/v1', // Change this to your Laravel API URL
   headers: {
     'Content-Type': 'application/json/multipart/form-data',
     'Accept': 'application/json',
